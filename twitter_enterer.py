3a18f5fe25fa447a7413542443d70cc276f1a8e9
nod32ba1 <52558002+nod32ba1@users.noreply.github.com>
2022-04-22 13:53:31 +0800

    Update twitter_enterer.py
    fix the SOL address reply

--- twitter_enterer.py
+++ twitter_enterer.py
@@ -43,9 +43,9 @@ class Enterer():
         if (any(word in tweet_content for word in ["drop", "comment", "put", "reply"])) and (any(w in tweet_content for w in ["address", "wallet", "$eth", "$sol","eth","sol"])):
 
             if(any(w in tweet_content for w in ["sol", "solana"])):
-                reply = random.choice([" SOL address : {}".format(self.eth_addr)," sol : {}".format(self.eth_addr), self.sol_addr ])
+                reply = random.choice(["SOL address : {}".format(self.sol_addr)," sol : {}".format(self.sol_addr), self.sol_addr ])
             else:
-                reply = random.choice([" ETH address : {}".format(self.eth_addr)," eth : {}".format(self.eth_addr), self.eth_addr ])
+                reply = random.choice(["ETH address : {}".format(self.eth_addr)," eth : {}".format(self.eth_addr), self.eth_addr ])
             self.client.create_tweet(text=reply, in_reply_to_tweet_id=tweet.id)
 
     # check if one the banned words is in the tweet
